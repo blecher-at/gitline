3d0877e4fe7389ff553137de5c0b698a4d772d78
Christoph Bonitz <ml.christophbonitz@gmail.com>
2015-11-07 22:44:32 +0100

    fixed package.json: npm start, npm test, npm run watch. Fixed test file

---
+++ src/test/gitlineModule.js
@@ -0,0 +1,4 @@
+var fs = require('fs'),
+  content = fs.readFileSync('./target/gitline.js', 'utf8');
+eval(content);
+exports.Commit = Commit;
--- src/test/index.js
+++ src/test/index.js
@@ -1,5 +1,10 @@
-var git2json = require("git2json");
-git2json._default(function(commits) {
-	console.log("%s", JSON.stringify(commits, null, 2));
- });
-
+var git2json = require('git2json'),
+  gitline = require('./gitlineModule'),
+	util = require('util');
+git2json.run(function(commits) {
+	var gitlineMock = {};
+	Object.keys(commits).forEach(function(sha) {
+		var currentCommit = new gitline.Commit(gitlineMock, commits[sha]);
+		console.log(util.inspect(currentCommit));
+	});
+});
