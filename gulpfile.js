37f63e378e621e2ade957b68eba65392690fd28d
Alex Gassner <office@alex-gassner.at>
2015-11-08 22:21:21 +0100

    close #30: added Sass support

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
+'use strict';
+
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var serve = require('gulp-serve');
 var mocha = require('gulp-mocha');
+var sass = require('gulp-sass');
 
 var tsProject = ts.createProject({
     outFile: 'gitline.js',
@@ -11,26 +14,44 @@ var tsProject = ts.createProject({
 });
 
 gulp.task('tsc', function () {
-    var tsResult = gulp.src('src/main/ts/*.ts')
+    var tsResult = gulp.src('src/main/ts/**/*.ts')
         .pipe(ts(tsProject, {}, ts.reporter.fullReporter(true)));
-    return tsResult.js.pipe(gulp.dest('target'));
+    return tsResult.js.pipe(gulp.dest('target/js'));
 });
 
 gulp.task('compress', ['tsc'], function () {
-    return gulp.src('target/gitline.js')
+    return gulp.src('target/js/gitline.js')
         .pipe(uglify())
         .pipe(rename({
             suffix: '.min'
         }))
-        .pipe(gulp.dest('target'));
+        .pipe(gulp.dest('target/js'));
 });
 
-gulp.task('build', ['compress']);
+gulp.task('sass:compressed', function () {
+    return gulp.src('src/main/scss/**/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('target/css'));
+});
+
+gulp.task('sass:uncompressed', function () {
+    return gulp.src('src/main/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('target/css'));
+});
+
+gulp.task('sass', ['sass:uncompressed', 'sass:compressed']);
+
+gulp.task('build', ['compress', 'sass']);
 
 gulp.task('run', ['build'], serve('.'));
 
 gulp.task('watch', ['build'], function () {
-    gulp.watch('src/main/ts/*.ts', ['build']);
+    gulp.watch('src/main/ts/**/*.ts', ['compress']);
+    gulp.watch('src/main/scss/**/*.scss', ['sass']);
 });
 
 gulp.task('test', function () {
