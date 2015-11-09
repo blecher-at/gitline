d3e29d95cb87fdb731ed61905fb95a3496f2ea4d
Stephan <token_github@blecher.at>
2015-11-09 20:46:25 +0100

    GL-9 travis logger support

--- src/test/helper/gitlineModule.js
+++ src/test/helper/gitlineModule.js
@@ -1,5 +1,7 @@
 var fs = require('fs'),
   content = fs.readFileSync('./target/gitline.js', 'utf8');
+  content += fs.readFileSync('./src/main/external/logger.min.js', 'utf8');
 eval(content);
+exports.Logger = Logger;
 exports.Commit = Commit;
 exports.Gitline = Gitline;
