2818795e704794c2c312ff6e90adee8f09f5e497
Stephan <token_github@blecher.at>
2015-11-14 15:09:09 +0100

    add deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+echo Running $0 from $(pwd) - for branch $1
 
-pwd
-ls -l
+mkdir .deploy
+cp -R ../dist .
+cd .deploy
 
+git init
+git config user.name "Travis-CI"
+git config user.email "token-travisci@blecher.at"
+git remote add target "http://$GH_TOKEN@github.com/blecher-at/gitline.git"
+git checkout $1
+
+cp -R ../dist .
+git add -A
+git commit -m "Automatic deploy of pages to $1"
+git push target HEAD
