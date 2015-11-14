8aee678fc0feccbb5d5a6eb4fc9ac056bd80d218
Stephan <token_github@blecher.at>
2015-11-14 14:22:27 +0100

    GL-36: test deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
 'use strict';
 
 var gulp = require('gulp');
+var del = require('del');
 var ts = require('gulp-typescript');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var serve = require('gulp-serve');
 var mocha = require('gulp-mocha');
 var sass = require('gulp-sass');
+//var git = require('gulp-git');
+//#var process = require('gulp-process');
 var sourcemaps = require('gulp-sourcemaps');
+var deploy = require('gulp-deploy-git');
+var all = require('gulp-all');
 
 var typescriptSource = 'src/main/ts/**/*.ts';
 var typeScriptTarget = 'target/js';
@@ -20,7 +25,16 @@ var tsProject = ts.createProject({
     noImplicitAny: false
 });
 
-gulp.task('tsc', function () {
+
+gulp.task('clean', function () {
+  return del([
+   	'.pages',
+	'dist',
+	'target/**/*'
+  ]);
+});
+
+gulp.task('tsc', ['clean'], function () {
     var tsResult = gulp.src(typescriptSource)
         .pipe(sourcemaps.init())
         .pipe(ts(tsProject, {}, ts.reporter.fullReporter(true)));
@@ -71,15 +85,27 @@ gulp.task('watch', ['build', 'test','package'], function () {
     gulp.watch(sassSource, ['sass', 'package']);
 });
 
-gulp.task('package', ['build', 'test'], function () {
-	gulp.src([ 'src/main/external/**']).pipe(gulp.dest('dist/external'));
-	gulp.src([ 'src/test/data/**']).pipe(gulp.dest('dist/data'));
-	gulp.src([
-		'target/gitline.min.js', 
-		'target/css/gitline.min.css', 
-		'src/main/external',
-		'src/demo/html/**/*']).pipe(gulp.dest('dist'));
+gulp.task('package', ['clean', 'test'], function () {
+	var p1 = gulp.src([
+	  		'target/js/gitline.min.js', 
+	  		'target/css/gitline.min.css', 
+	  		'src/demo/html/**']).pipe(gulp.dest('dist'));
+	var p2 = gulp.src([ 'src/main/external/**']).pipe(gulp.dest('dist/external'));
+	var p3 = gulp.src([ 'src/test/data/**']).pipe(gulp.dest('dist/data'));
+	return all(p1, p2, p3);
 });
+
+gulp.task('deploy', ['package'], function() {
+  return gulp.src('dist/**/*')
+    .pipe(deploy({
+      repository: 'git@github.com:blecher-at/gitline.git',
+	branches: ['feature/GL-36-dist', 'master'],
+	message: 'auto deploy',
+	debug: true,
+	remoteBranch: 'gh-pages-test'
+    }));
+});
+
 gulp.task('test', ['build'], function () {
     return gulp.src('src/test/GitlineTests.js', {read: false})
         // gulp-mocha needs filepaths so you can't have any plugins before it
